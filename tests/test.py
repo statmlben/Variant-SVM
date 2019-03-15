27871ec785eccec203f37a2220ceb0bd2a211568
Ben Dai <bendai2-c@my.cityu.edu.hk>
2019-03-15 16:02:25 -0500

    modified:   VarSVM/.DS_Store 	modified:   tests/test.py

--- tests/test.py
+++ tests/test.py
@@ -1,12 +1,13 @@
 import numpy as np
 from sklearn.datasets import make_classification
-from dblearn import DriftSVM, NoNeg_DriftSVM
+from VarSVM import noneg_driftsvm
+
 X, y = make_classification(n_features=4, random_state=0)
 y = y * 2 - 1
 
 n = len(X)
 drift = .28*np.ones(n)
 
-clf = NoNeg_DriftSVM()
+clf = noneg_driftsvm()
 clf.fit(X=X, y=y, drift=drift)
 clf.decision_function(X=X, drift=drift) * y
