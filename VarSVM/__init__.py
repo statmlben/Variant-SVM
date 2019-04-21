503df2f8d52191b3107f8e1c33327aa051182b6a
statmlben <bendaimath@gmail.com>
2019-04-20 23:39:09 -0500

    Add existing file

--- VarSVM/__init__.py
+++ VarSVM/__init__.py
@@ -1,6 +1,6 @@
 from .fastloop import CD, CD_drift, noneg_CD_drift
-# from VarSVM import weightsvm
-# from VarSVM import driftsvm
-# from VarSVM import noneg_driftsvm
+from VarSVM import weightsvm
+from VarSVM import driftsvm
+from VarSVM import noneg_driftsvm
 
 # __all__ = ['fastloop', 'weightsvm', 'driftsvm', 'noneg_driftsvm']
