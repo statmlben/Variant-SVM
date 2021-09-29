2104e8c72e06d952bf62fba8e11de43117e102ad
Ben Dai <bendaimath@gmail.com>
2021-09-29 20:37:46 +0800

    update RM

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ ext_1 = Extension(SRC_DIR + ".fastloop",
                   libraries=[],
                   include_dirs=[np.get_include()])
 
-with open('README.rst') as f:
-    LONG_DESCRIPTION = f.read()
+# with open('README.rst') as f:
+#     LONG_DESCRIPTION = f.read()
 
 EXTENSIONS = [ext_1]
 
@@ -20,18 +20,18 @@ if __name__ == "__main__":
         description='Python library for Variants of Support Vector Machines',
         url='https://github.com/statmlben/Variant-SVM',
         author='Ben Dai',
-        author_email='bdai@umn.edu',
+        author_email='bendai@cuhk.edu.hk',
         # Needed to actually package something
         packages=['varsvm'],
         # Needed for dependencies
         install_requires=['numpy', 'scipy', 'Cython'],
         # *strongly* suggested for sharing
-        version='1.3',
-        download_url = 'https://github.com/statmlben/Variant-SVM/archive/1.3.tar.gz',
+        version='1.4',
+        download_url = 'https://github.com/statmlben/Variant-SVM/archive/1.4.tar.gz',
         # The license can be anything you like
         license='MIT',
         #cmdclass={"build_ext": build_ext},
         ext_modules=EXTENSIONS,
         # We will also need a readme eventually (there will be a warning)
-        long_description=LONG_DESCRIPTION
+        # long_description=LONG_DESCRIPTION
     )
